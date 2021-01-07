1dbc06ffb9a9dba67936c4b1f4a612da0fd6a528
tagirahmad <tagirbpl@gmail.com>
2021-01-07 20:14:54 +0300

    many improvements

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase if email }
-  has_many :articles
+  has_many :articles, dependent: :destroy
   validates :username, presence: true,
                        uniqueness: { case_sensitive: true }, length: { minimum: 3, maximum: 25 }
 
@@ -8,6 +8,6 @@ class User < ApplicationRecord
   validates :email, presence: true,
                     uniqueness: { case_sensitive: true }, length: { maximum: 105 },
                     format: { with: VALID_EMAIL_REGEX }
-  
+
   has_secure_password
 end
